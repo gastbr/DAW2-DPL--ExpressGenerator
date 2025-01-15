aee1de5c9547b6f7196196f8431407bcf0d66c68
AlumG <AlumG@majada.es>
2025-01-15 20:21:45 +0100

    A25 pending

--- app.js
+++ app.js
@@ -6,6 +6,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var bicicletasAPIRouter = require('./routes/api/bicicletas');
 
 var app = express();
 
@@ -19,16 +20,19 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//If there are several versions of the API, /api/v1/bicicletas is usually used
+app.use('/api/bicicletas', bicicletasAPIRouter);
+
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
