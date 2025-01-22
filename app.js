4451a8aa4f63924af786a2e3bb412a902b65d2e5
AlumG <AlumG@majada.es>
2025-01-22 20:13:26 +0100

    A26 finished

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var swaggerJsdoc = require("swagger-jsdoc");
+var swaggerUi = require("swagger-ui-express");
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -20,11 +22,48 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+const options = {
+  definition: {
+    openapi: "3.1.0",
+    info: {
+      title: "API Bicicletas",
+      version: "0.1.0",
+      description:
+        "Una API para administrar bicicletas",
+      license: {
+        name: "MIT",
+        url: "https://spdx.org/licenses/MIT.html",
+      },
+      contact: {
+        name: "LogRocket",
+        url: "https://logrocket.com",
+        email: "info@email.com",
+      },
+    },
+    servers: [
+      {
+        url: "http://localhost:3000",
+      },
+    ],
+  },
+  apis: ["./models/*.js"],
+};
+
+const specs = swaggerJsdoc(options);
+
+
 //If there are several versions of the API, /api/v1/bicicletas is usually used
 app.use('/api/bicicletas', bicicletasAPIRouter);
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use(
+  "/api-docs",
+  swaggerUi.serve,
+  swaggerUi.setup(specs, {
+    explorer: true,
+    customCssUrl: "https://cdn.jsdelivr.net/npm/swagger-ui-themes@3.0.0/themes/3.x/theme-newspaper.css",
+  })
+);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
--- models/Bicicleta.js
+++ models/Bicicleta.js
@@ -1,3 +1,136 @@
+/**
+ * @swagger
+*components:
+*  schemas:
+*    Bicicleta:
+*      type: object
+*      required:
+*      - ubicacion
+*      properties:
+*        id:
+*          type: number
+*          description: The auto-generated id of the bike
+*        color:
+*          type: string
+*          description: The color of your bike
+*        modelo:
+*          type: string
+*          description: The bike model
+*        ubicacion:
+*          type: array
+*          items:
+*            type: number
+*          description: Where the bike is located
+*      example:
+*        id: 1
+*        color: red
+*        modelo: Trek
+*        ubicacion: [ 28.503789, -13.853296 ]
+*
+ */
+
+/**
+ * @swagger
+ * tags:
+ *   name: Bicicletas
+ *   description: The bikes managing API
+ * /bicicletas:
+ *   get:
+ *     summary: Lists all the bikes
+ *     tags: [Sin ID]
+ *     responses:
+ *       200:
+ *         description: The list of the bikes
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: array
+ *               items:
+ *                 $ref: '#/components/schemas/Bicicleta'
+ *   post:
+ *     summary: Create a new bike
+ *     tags: [Sin ID]
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             $ref: '#/components/schemas/Bicicleta'
+ *     responses:
+ *       200:
+ *         description: The created bike.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Bicicleta'
+ *       500:
+ *         description: Error al añadir bicicleta
+ * /bicicletas/{id}:
+ *   get:
+ *     summary: Get the bike by id
+ *     tags: [Con ID]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The bike id
+ *     responses:
+ *       200:
+ *         description: The bike response by id
+ *         contens:
+ *           application/json:
+ *             schema:
+ *               $ref: '#/components/schemas/Bicicleta'
+ *       404:
+ *         description: The bike was not found
+ *   put:
+ *    summary: Update the bike by the id
+ *    tags: [Con ID]
+ *    parameters:
+ *      - in: path
+ *        name: id
+ *        schema:
+ *          type: string
+ *        required: true
+ *        description: The bike id
+ *    requestBody:
+ *      required: true
+ *      content:
+ *        application/json:
+ *          schema:
+ *            $ref: '#/components/schemas/Bicicleta'
+ *    responses:
+ *      200:
+ *        description: The bike was updated
+ *        content:
+ *          application/json:
+ *            schema:
+ *              $ref: '#/components/schemas/Bicicleta'
+ *      404:
+ *        description: The bike was not found
+ *      500:
+ *        description: Error al actualizar la bicicleta
+ *   delete:
+ *     summary: Remove the bike by id
+ *     tags: [Con ID]
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         schema:
+ *           type: string
+ *         required: true
+ *         description: The bike id
+ *
+ *     responses:
+ *       200:
+ *         description: The bike was deleted
+ *       404:
+ *         description: The bike was not found
+ */
+
+
 let Bicicleta = function (id, color, modelo, ubicacion) {
     this.id = id;
     this.color = color;
